807048067ac76d000d396b67f246e30e0558127c
theblue-bay <86391514+theblue-bay@users.noreply.github.com>
2022-03-01 11:20:06 -0600

    completed hw 4 including test cases

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,14 +1,5 @@
 #include "decisions.h"
-//write include statement for decisions header
 
-//Write code for function get_grade_points that accepts a string letter_grade and returns 
-//the grade_points for as follows:
-//given grade "A" returns 4
-//given grade "B" returns 3
-//given grade "C" returns 2
-//given grade "D" returns 1
-//given grade "F" returns 0
-//another other value return -1
 int get_grade_points(std::string letter_grade)
 {
 	if (letter_grade == "A") 
--- src/homework/04_iteration/dna.h
+++ src/homework/04_iteration/dna.h
@@ -1,19 +1,4 @@
-#include<string>
 
-/*
-Write prototype for function get_gc_content that accepts
-a const reference string parameter and returns a double
-*/
+int factorial(int num);
 
-
-/*
-Write prototype for function get_reverse_string that 
-accepts a string parameter and returns a string
-*/
-
-
-/*
-Write prototype for function get_dna_complement that
-accepts a string and returns a string
-*/
-
+int gcd(int num2, int num3);
