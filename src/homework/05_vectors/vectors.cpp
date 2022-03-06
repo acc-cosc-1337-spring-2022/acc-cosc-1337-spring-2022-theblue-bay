0d16f136823f8d794bc99fe45b1e333c303f6ef1
theblue-bay <86391514+theblue-bay@users.noreply.github.com>
2022-03-06 01:43:07 -0600

    help

--- src/homework/05_vectors/vectors.cpp
+++ src/homework/05_vectors/vectors.cpp
@@ -1,30 +1,56 @@
-/*
-Write a value return function get_max_from_vector with a const reference 
-vector of intsparameter that returns the max value in a vector
+#include "string"
+#include "vectors.h"
+#include "algorithm"
 
-@param numbers is a const reference to a vector of integers
-@return the max value in the vector
-*/
+double getGcContent(const std::string &dna)
+{
+    size_t c = std::count(dna.begin(), dna.end(), 'c');
+    size_t g = std::count(dna.begin(), dna.end(), 'g');
+
+    double total = c + g;
+
+    double dnaLength = dna.length();
+
+    double percent = total / dnaLength;
+
+    return percent;
+}
+
+std::string getReverseString(std::string dna)
+{
+    std::reverse(dna.begin(), dna.end());
+
+    return dna;
+}
+
+std::string getDnaCompliment(const std::string  &dna)
+{
+    std::string complement = "";
+
+    for (int i = dna.length()-1; i >= 0; i--)
+    {
+        switch(dna[i]) 
+        {
+            case 'g': {
+                complement = complement + 'c';
+                break;
+            }
+            case 'c': {
+                complement = complement + 'g';
+                break;
+            }
+            case 't': {
+                complement = complement + 'a';
+                break;
+            }
+            case 'a': {
+                complement = complement + 't';
+                break;
+            }
+        }
+    }
+    return dna;
 
 
-
-/*
-Write a function named is_prime with an integer parameter that
-given a number returns true if prime or false if not prime
-
-@param number: Any whole number
-@return: bool if prime False if not
-*/
-
-/*
-Write a a function named vector_of_primes with an integer parameter
-that given a number returns all the prime numbers up to the number
-
-Example given number 10 returns a vector with elements 2,3,5,7,
-
-@param int integer value
-@return vector of ints containing prime values up to a value
-
-Make sure to use the is_prime function to determine if current 
-number is prime.
-*/
+    
+}
